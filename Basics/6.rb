8e558b23f0766074873cfe6ffefbde17d1b342b3
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-02-03 12:42:43 +0000

    exercise 6

---
+++ Basics/6.rb
@@ -0,0 +1,6 @@
+#Write a program that calculates the squares of 3 float numbers of your choosing 
+#and outputs the result to the screen.
+
+puts "Square of 3.3 is #{3.3*3.3}"
+puts "Square of 8.1 is #{8.1*8.1}"
+puts "Square of 5.32 is #{5.32*5.32}"
