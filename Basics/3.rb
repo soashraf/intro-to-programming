f289c25807e5b1b106e0bed3a633895e53dfedd0
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-02-03 12:27:37 +0000

    exercise 3

---
+++ Basics/3.rb
@@ -0,0 +1,9 @@
+# Write a program that uses a hash to store a list of movie titles with the year 
+# they came out. Then use the puts command to make your program print out the year 
+# of each movie to the screen. The output for your program should look something like this.
+
+movies = {:Superman => 1978, :Avengers => 2012, :IronMan => 2008, :Hulk => 2003}
+puts movies[:Superman]
+puts movies[:Avengers]
+puts movies[:IronMan]
+puts movies[:Hulk]
