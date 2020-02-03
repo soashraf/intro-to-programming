2eb8b3b07ba1af32776cfa1ba699b10ba54ca044
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-02-03 12:31:27 +0000

    exercise 4

---
+++ Basics/4.rb
@@ -0,0 +1,8 @@
+#Use the dates from the previous example and store them in an array. 
+#Then make your program output the same thing as exercise 3.
+
+dates = [1978, 2012, 2008, 2003]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
