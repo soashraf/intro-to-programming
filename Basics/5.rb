875bcc29604afdb411d14fb42c85bb0ecb370f68
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-02-03 12:38:51 +0000

    exercise 5

---
+++ Basics/5.rb
@@ -0,0 +1,6 @@
+#Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+
+puts "Factorial 5 is #{5*4*3*2*1}"
+puts "Factorial 6 is #{6*5*4*3*2*1}"
+puts "Factorial 7 is #{7*6*5*4*3*2*1}"
+puts "Factorial 8 is #{8*7*6*5*4*3*2*1}"
