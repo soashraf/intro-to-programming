5d8e3f33580ae8ff8ef23620cefff223cb24e03b
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-02-03 22:15:41 +0000

    methods section

---
+++ Methods/2.rb
@@ -0,0 +1,15 @@
+#What do the following expressions evaluate to?
+#1. x = 2
+puts "x=2 evaluates to 2"
+
+#2. puts x = 2
+puts "puts x=2 evaluates to nil"
+
+#3. p name = "Joe"
+puts "p name = \"Joe\" evaluates to \"Joe\""
+
+#4. four = "four"
+puts "four = \"four\" evaluates to \"four\""
+
+#5. print something = "nothing"
+puts "print something = \"nothing\" evaluates to nil"
---
+++ Methods/5.rb
@@ -0,0 +1,10 @@
+#1) Edit the method definition in exercise #4 so that it does print words on the screen. 
+#2) What does it return now?
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+puts "The method returns nil"
