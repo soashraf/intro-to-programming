6888adeba03a023f153f748efcd2196a3809d436
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-08-20 15:26:31 +0100

    Exercises, ex 16

---
+++ Exercises/16.rb
@@ -0,0 +1,25 @@
+#Given the following data structures. Write a program that copies the information from the array 
+#into the empty hash that applies to the correct person.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+# Now, programmatically loop or iterate over the contacts hash from exercise 11, 
+# and populate the associated data from the contact_data array. 
+# Hint: you will probably need to iterate over ([:email, :address, :phone]), 
+# and some helpful methods might be the Array shift and first methods.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each_with_index do |(name, hash), idx|
+  fields.each do |field|
+    hash[field] = contact_data[idx].shift
+  end
+end
+
+puts contacts
